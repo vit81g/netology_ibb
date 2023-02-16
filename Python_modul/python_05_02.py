6a5d95d68be31d14a0efc5b9bc734d88bb41aeb8
vit81g <spellhvit@gmail.com>
2023-02-16 14:03:45 +0300

    pyyhon05_02_3

--- Python_modul/python_05_02.py
+++ Python_modul/python_05_02.py
@@ -31,7 +31,7 @@ def funcDeleteNomber():
     """
     d – delete – команда, которая спросит номер документа и удалит его из каталога и из перечня полок.
     Предусмотрите сценарий, когда пользователь вводит несуществующий документ;
-    :return:
+    :return: documents
     """
     delNumber = input('Введите номер документа: ')
     for i in documents:
@@ -50,7 +50,7 @@ def funcMoveNumber():
     m – move – команда, которая спросит номер документа и целевую полку и переместит его с текущей полки на целевую.
     Корректно обработайте кейсы, когда пользователь пытается переместить несуществующий документ или
     переместить документ на несуществующую полку;
-    :return:
+    :return: directories
     """
     doc_number = input('Введите номер документа: ')
     new_directories = input('Введите новую полку, куда переместить документ: ')
@@ -61,7 +61,7 @@ def funcMoveNumber():
 
             # добавление элемента во временную переменную
                 new_list.append(doc_number)
-                print('перебор k')
+                # print('перебор k')
                 for k in directories:
                     # удаление элемента
                     if doc_number in directories[k]:
@@ -75,13 +75,29 @@ def funcMoveNumber():
     return f'{directories}'
 
 def funcAddDirectories():
-    pass
+    """
+    as – add shelf – команда, которая спросит номер новой полки и добавит ее в перечень.
+    Предусмотрите случай, когда пользователь добавляет полку, которая уже существует.;
+    :return:
+    """
+
+    new_directories = input('Введите новую полку: ')
+    if new_directories in directories.keys():
+        print('Полка с таким номером уже существует')
+
+    else:
+        directories[new_directories] = []
+
+    return f'{directories}'
 
 if inputCommand == 'd':
     print(funcDeleteNomber())
 
 if inputCommand == 'm':
     print(funcMoveNumber())
+
+if inputCommand == 'as':
+    print(funcAddDirectories())
 
 
 else:
